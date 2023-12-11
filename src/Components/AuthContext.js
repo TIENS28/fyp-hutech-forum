8a31595bd8477ea1b1c0a81f1a96e9eb9029bea2
TIENS28 <105785161+TIENS28@users.noreply.github.com>
2023-12-11 22:09:44 +0700

    Add AuthContext.js

---
+++ src/Components/AuthContext.js
@@ -0,0 +1,59 @@
+// import React, { createContext, useContext, useState } from 'react';
+
+// const AuthContext = createContext();
+
+// export const AuthProvider = ({ children }) => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+//   const login = async (username, password) => {
+//     try {
+//       const response = await fetch('http://localhost:5001/api/auth/login', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           email: username,
+//           password: password,
+//         }),
+//       });
+  
+//       if (response.ok) {
+//         const data = await response.json();
+//         const token = data.token;
+  
+//         localStorage.setItem('token', token);
+  
+//         setIsAuthenticated(true);
+  
+//         return true;
+//       } else {
+//         console.error('Login failed.');
+//         return false;
+//       }
+//     } catch (error) {
+//       console.error('Error during login:', error);
+//       return false;
+//     }
+//   };
+
+//   const logout = () => {
+//     localStorage.removeItem('token');
+//     setIsAuthenticated(false);
+//   };
+
+//   return (
+//     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export const useAuth = () => {
+//     const context = useContext(AuthContext);
+//     if (!context) {
+//       throw new Error('useAuth must be used within an AuthProvider');
+//     }
+//     return context;
+//   };
+
