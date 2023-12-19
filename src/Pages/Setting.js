ff5818335cbd46d2f7c3f25ec46086bbbd252eb5
TIENS28 <105785161+TIENS28@users.noreply.github.com>
2023-12-20 00:00:27 +0700

    nothing

--- src/Pages/Setting.js
+++ src/Pages/Setting.js
@@ -5,14 +5,14 @@ import './Setting.css';
 import { useUser } from '../Components/UserContext';
 import { MdEdit } from "react-icons/md";
 import { FaSignOutAlt }    from "react-icons/fa";
-
+import { MdDelete } from "react-icons/md";
 
 function Setting() {
-    // ReactJS 
+    const { user } = useUser();
     const [toggle, setToggle] = useState(1);
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
     const navigate = useNavigate();
-
+    console.log(user);
     function updateToggle(id) {
       setToggle(id)
     }
@@ -35,6 +35,7 @@ function Setting() {
             </ul>
             <hr className='setting-hr'/>
       </nav>
+
       <div className={toggle === 1 ? "show-settingss" : "settingss"}>
           <h2 className='setting-h2'>Account</h2>
           <div className='form-setting'>
@@ -58,25 +59,10 @@ function Setting() {
                 <span className='setting-edit'><MdEdit /></span>
             </div>
             <hr className='setting-hr'/>
-
-            <p className='setting-text'>Password</p>
+            
+            <p className='setting-text'>DOB</p>
             <div className='setting-information'>
-                <p className='setting-texts'>{user.password}</p>
-                <span className='setting-edit'><MdEdit /></span>
-            </div>
-            <hr className='setting-hr'/>
-
-            <button onClick={()=>{navigate('/login', {replace:true})}}
-            className='bt-setting-logout'><FaSignOutAlt style={{ marginRight: '5px' }}/>Logout</button>
-          </div>
-      </div>
-
-      <div className={toggle === 2 ? "show-settingss" : "settingss"}>
-          <h2 className='setting-h2'>My Profile</h2>
-          <div className='form-setting'>
-            <p className='setting-text'>Full Name</p>
-            <div className='setting-information'>
-                <p className='setting-texts'>Nguyen Trang Chi Kiem</p>
+                <p className='setting-texts'>{user.DOB}</p>
                 <span className='setting-edit'><MdEdit /></span>
             </div>
             <hr className='setting-hr'/>
@@ -122,21 +108,15 @@ function Setting() {
                     {/* <span className='setting-edit'><MdEdit /></span> */}
                 </div>
                 <hr className='setting-hr'/>
-  
-
-            </div>
-            <div className='bt-settings'>
-        <button onClick={()=>{navigate('/homepage', {replace:true})}}
-                className='bt-setting-homepage'><FaHome style={{ marginRight: '5px' }}/>Homepage</button>
-      
+              </div>
+          </div>
             <button onClick={()=>{navigate('/login', {replace:true})}}
             className='bt-setting-logout'><FaSignOutAlt style={{ marginRight: '5px' }}/>Logout</button>
             <button onClick={()=>handleDeleteAccount()}
                 className='bt-setting-delete'><MdDelete style={{ marginRight: '5px' }}/>
                 Delete Accont
           </button>
-      </div>
-
+          </div>
       </div>
 
     </div>
